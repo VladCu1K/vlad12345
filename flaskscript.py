c718a37815b84b827b8a31187e017fcb6c2c92ee
VladCu1K <108799770+VladCu1K@users.noreply.github.com>
2022-08-29 13:21:18 +0300

    Update flaskscript.py

--- flaskscript.py
+++ flaskscript.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloIndex():
-    return ' 123 123 1231 3 '
+    return ' 1asdffffff'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
